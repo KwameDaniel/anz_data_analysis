44147f2b9bb4da268ca509c8252dc06e38771a89
Kwame <kenyemkwamedaniel@gmail.com>
2020-06-30 14:12:00 +0100

    my first commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Sales anaysis EDA and Predictive Analytics[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[C[C[C[C[C[C[C[[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C EDA and Predictive Analytics on Sales data',
+    author='Kwame',
+    license='MIT',
+)
